619d558edd232d526d481784be7327a468422964
Jerson_Ferrer <jferrer00747@gmail.com>
2020-05-04 14:43:18 -0500

    Se mejoró el diseño de la cinta

--- js/horizontal.js
+++ js/horizontal.js
@@ -7,7 +7,7 @@ jQuery(function($){
 	(function () {
 		var $frame = $('#forcecentered');
 		var $wrap  = $frame.parent();
-		var $controls = $('.controls')
+		var $controls = $('.control-buttons')
 
 		// Call Sly on frame
 		$frame.sly({
@@ -19,7 +19,7 @@ jQuery(function($){
 			mouseDragging: 1,
 			touchDragging: 1,
 			releaseSwing: 1,
-			startAt: 5,
+			startAt: 11,
 			scrollBar: $wrap.find('.scrollbar'),
 			scrollBy: 1,
 			speed: 300,
