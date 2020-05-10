4c03f010069c30fcab3dc9066237cf13a943050d
DevinsonTrigos <61364664+DevinsonTrigos@users.noreply.github.com>
2020-05-09 19:17:30 -0500

    Animacion de celdas
    Movimientos de celdas

--- js/M_turing.js
+++ js/M_turing.js
@@ -1,194 +1,250 @@
 
 //Los String en Js son Inmutables, por esa razón se hace este subcódigo para reemplazarlos por una copia 
-String.prototype.replaceAt=function(index, replacement) {
-    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+String.prototype.replaceAt = function (index, replacement) {
+  return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 }
 
-let cuadro = document.getElementById ('cuadro');
+let cuadro = document.getElementById('cuadro');
 //var pasos = document.getElementById ('pasos');
 //var estado = document.getElementById ('estado');
-var palabra= document.getElementById("Word").value;
+var palabra = document.getElementById("Word").value;
 
-ind_estado = 0 ;
+ind_estado = 0;
 
-//velocidad de cambio
+                       
 
-function move_left(cont){
+function move_left(cont) {
 
-    console.log(parseInt(slider.noUiSlider.get(), 10));
+  console.log(parseInt(slider.noUiSlider.get(), 10));
 
-    $( ".item" ).animate({ "left": "+=50px" },parseInt(slider.noUiSlider.get(), 10));
+  $(".item").animate({ "left": "+=50px" }, parseInt(slider.noUiSlider.get(), 10));
 
-    //$( ".item" ).animate({ "left": "+=50px" },speed);
+  $("#estado").animate({ "left": "+=0px" }, parseInt(slider.noUiSlider.get(), 10));
+  $("#estado").fadeIn(function () {
+    $(this).html("Estado: q_2 ").fadeIn();
+  });
 
-    $( "#estado" ).animate({ "left": "+=0px" },parseInt(slider.noUiSlider.get(), 10));
-    $("#estado").fadeIn(function() {
-        $(this).html("Estado: q_2 ").fadeIn();
-      });
-   
-    $( "#pasos" ).animate({ "left": "+=0px" },parseInt(slider.noUiSlider.get(), 10));
-    $("#pasos").fadeIn(function() {
-        $(this).html("Pasos: "+(cont+1)).fadeIn();
-      });
+  $("#pasos").animate({ "left": "+=0px" }, parseInt(slider.noUiSlider.get(), 10));
+  $("#pasos").fadeIn(function () {
+    $(this).html("Pasos: " + (cont + 1)).fadeIn();
+  });
 
-   
+
 
 }
 
 
-function move_right(cont){
+function move_right(cont) {
 
-    console.log(parseInt(slider.noUiSlider.get(), 10));
+  console.log(parseInt(slider.noUiSlider.get(), 10));
 
-    $( ".item" ).animate({ "left": "-=50px" },parseInt(slider.noUiSlider.get(), 10));
-   
-    //$( ".item" ).animate({ "left": "-=50px" },speed);
-    
-    if( ind_estado == 3 ){
-        $( "#estado" ).animate({ "left": "+=0px" },parseInt(slider.noUiSlider.get(), 10));
-        $("#estado").fadeIn(function() {
-            $(this).html("Estado: q_3 ").fadeIn();
-          });    
-    }else{
-        $( "#estado" ).animate({ "left": "+=0px" },parseInt(slider.noUiSlider.get(), 10));
-        $("#estado").fadeIn(function() {
-            $(this).html("Estado: q_1 ").fadeIn();
-          });
-    }
-   
-    $( "#pasos" ).animate({ "left": "+=0px" },parseInt(slider.noUiSlider.get(), 10));
-    $("#pasos").fadeIn(function() {
-        $(this).html("Pasos: "+(cont+1)).fadeIn();
-      });
+  $(".item").animate({ "left": "-=50px" }, parseInt(slider.noUiSlider.get(), 10));
 
-       
+  if (ind_estado == 3) {
+    $("#estado").animate({ "left": "+=0px" }, parseInt(slider.noUiSlider.get(), 10));
+    $("#estado").fadeIn(function () {
+      $(this).html("Estado: q_3 ").fadeIn();
+    });
+  } else {
+    $("#estado").animate({ "left": "+=0px" }, parseInt(slider.noUiSlider.get(), 10));
+    $("#estado").fadeIn(function () {
+      $(this).html("Estado: q_1 ").fadeIn();
+    });
+  }
+
+  $("#pasos").animate({ "left": "+=0px" }, parseInt(slider.noUiSlider.get(), 10));
+  $("#pasos").fadeIn(function () {
+    $(this).html("Pasos: " + (cont + 1)).fadeIn();
+  });
+
 }
+
+function paint(estado,a,b){
+
+  if(estado == 1 || estado == 3){
+
+       $(a).fadeIn(function () {
+         $(this).css("backgroundColor", "#951197 ");
+       });
+       $(b).fadeIn(function () {
+         $(this).css("backgroundColor", "#333 ");
+       });
+  }else if(estado== 2){
+    $(a).fadeIn(function () {
+      $(this).css("backgroundColor", "#951197");
+    });
+    $(b).fadeIn(function () {
+      $(this).css("backgroundColor", "#333 ");
+    });
+  }
+}
+
 
 function run() {
+  let intervalo;
+  var word = document.getElementById("Word").value;
+  document.getElementById("btnp").disabled = false;
+  document.getElementById("btns").disabled = false;
 
-    var word= document.getElementById("Word").value;
-    
-    /*declaración de variables que operan en la máquina de turing y sus movimientos, n es el encargado de moverse
-    dentro del String*/
-    let n=1;
-    let estado=1;
-    right=0;
-    left=0;
-    sw=1;
-    
-    //Variables de Movimiento
-    posicion=10;
-    cont = 0 ;
-    
-    //Ciclo que controla todo el movimiento y finaliza en el estado de aceptación//
-      if(word != ""){
-        word='z'+word+'z';
-            while(estado!=3){
-                  //________Controla el estado Q1 y sus movimientos//
-                        while( estado==1){
-    
-                            ind_estado = 1;
-                            
-                            if(word.charAt(n)=="a"){
-                                 
-        
-                                  $("#cuadro"+posicion).fadeIn(function() {
-                                      $(this).html("<h1>A</h1>").fadeIn();
-                                  });
-    
-                                  word=word.replaceAt(n, "a");
-                                    
-                                  move_right(cont);
-    
-                                   
-    
-                                    cont++;
-                                    right++;
-                                    posicion++;
-                            }else if(word.charAt(n)=="b"){
-                                
-                                
-                                  $("#cuadro"+posicion).fadeIn(function() {
-                                      $(this).html("<h1>A</h1>").fadeIn();
-                                    });
-                               
-                                  word=word.replaceAt(n, "a");
-                                  
-                                  move_right(cont);
-    
-                                                                
-                                  cont++;
-                                  right++;
-                                  posicion++;
-                                        
-                            }else if(word.charAt(n)=="z"){                          
-                                            
-                                  word=word.replaceAt(n, "a");
-                                  
-                                  move_left(cont);
-                                           
-                                      estado=2;
-                                      left++;
-                                      n--;
-                                                
-                                      sw=0;
-                                   cont++;
-                                                
-                           }                                
-                        
-                         /*Como hemos restado n, para no tener un movimento de 2 a la derecha
-                          y uno a izquierda,   se establece un sw*/
-                         if (sw==1){  n++;  }
-                                
-                        }
-                       
-                        sw=1;
-                        posicion--;
-                        //________________Controla el estado Q2 y sus movimientos__________*/
-                        while(estado==2){
-    
-                           
-    
-                            if(word.charAt(n)=="a"){
-                                $("#cuadro"+posicion).fadeIn(function() {
-                                    $(this).html("<h1>a</h1>").fadeIn();
-                                  });
-    
-                                word=word.replaceAt(n, "a");
-                                left++;
-                                posicion--
-                                move_left(cont);
-                                
-                                cont++;
-                                
-                            }else if(word.charAt(n)=="z"){
-    
-                               ind_estado = 3;
-    
-                               word=word.replaceAt(n,"a");
-                               estado=3;
-                               right++;
-                               n++;
-                               sw=0;
-                               posicion--
-                               move_right(cont);                       
-                              
-                               cont++;     
-                                
-                            }
-                            if(sw==1){
-                            n--;
-                            
-                            }
-                            }
-    
-                        }
-    
-        }else{
-            alert("No puedes jugar si está vacía la expresión");
+  /*declaración de variables que operan en la máquina de turing y sus movimientos, n es el encargado de moverse
+  dentro del String*/
+  let n = 1;
+  let estado = 1;
+  right = 0;
+  left = 0;
+  sw = 1;
+
+  //Variables de Movimiento
+  posicion = 12;
+  cont = 0;
+//Condicion que controla todo el ciclo de el movimiento y finaliza en el estado de aceptación//
+  if (word != "") {
+    word = 'z' + word + 'z';
+    while (estado != 3) {
+      //________Controla el estado Q1 y sus movimientos//
+      while (estado == 1) {
+
+        ind_estado = 1;
+
+        if (word.charAt(n) == "a") {
+
+          $("#cuadro" + posicion).fadeIn(function () {
+            $(this).html("<h1>a</h1>").fadeIn();
+          });
+
+          word = word.replaceAt(n, "a");
+
+          id1 ="#cuadro"+(posicion+1);
+          id2 ="#cuadro"+(posicion);
+          paint(ind_estado,id1,id2);
+
+          move_right(cont);
+
+          cont++;
+          right++;
+          posicion++;
+
+        } else if (word.charAt(n) == "b") {
+
+
+          $("#cuadro" + posicion).fadeIn(function () {
+            $(this).html("<h1>a</h1>").fadeIn();
+          });
+
+          id1 ="#cuadro"+(posicion+1);
+          id2 ="#cuadro"+(posicion);
+          paint(ind_estado,id1,id2);
+
+          word = word.replaceAt(n, "a");
+
+          move_right(cont);
+
+          cont++;
+          right++;
+          posicion++;
+
+        } else if (word.charAt(n) == "z") {
+
+          word = word.replaceAt(n, "a");
+
+          $("#cuadro" + posicion).fadeIn(function () {
+            $(this).html("<h1></h1>").fadeIn();
+          });
+
+          id1 ="#cuadro"+(posicion-1);
+          id2 ="#cuadro"+(posicion);
+          paint(ind_estado,id1,id2);
+
+
+          move_left(cont);
+
+          estado = 2;
+          left++;
+          n--;
+
+          sw = 0;
+          cont++;
+
         }
+
+        /*Como hemos restado n, para no tener un movimento de 2 a la derecha
+         y uno a izquierda,   se establece un sw*/
+        if (sw == 1) { n++; }
+
+      }
+
+      id1 ="#cuadro"+(posicion);
+      $(id1).fadeIn(function () {
+        $(this).css("backgroundColor", "#333 ");
+      });
+
+      sw = 1;
+      posicion--;
+      //________________Controla el estado Q2 y sus movimientos__________*/
+      while (estado == 2) {
+
+        if (word.charAt(n) == "a") {
+
+          ind_estado = 2;
+
+          word = word.replaceAt(n, "a");
+
+          $("#cuadro" + posicion).fadeIn(function () {
+            $(this).html("<h1>a</h1>").fadeIn();
+          });
+
+          id1 ="#cuadro"+(posicion-1);
+          id2 ="#cuadro"+posicion;
+          paint(ind_estado,id1,id2);
+
+          left++;
+          posicion--;
+          move_left(cont);
+          cont++;
+
+        } else if (word.charAt(n) == "z") {
+
+          ind_estado = 3;
+
+          word = word.replaceAt(n, "a");
+
+          id1 ="#cuadro"+(posicion+1);
+          id2 ="#cuadro"+posicion;
+          paint(ind_estado,id1,id2);
+
+          estado = 3;
+          right++;
+          n++;
+          sw = 0;
+          posicion--
+          move_right(cont);
+
+          cont++;
+
+        }
+        if (sw == 1) {
+          n--;
+
+        }
+      }
+
     }
-    
 
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'No puede jugar sin ninguna palabra cargada',
+      confirmButtonText: 'Aceptar',
+      //footer: '<a href="../../vista/pagina/login.php">Why do I have this issue?</a>'
+    }).then((result) => {
+      if (result.value) {
+        Swal.close("salir");        
+      }
+    });
+  }
+}
+function pausar() {
+  window.alert("Se ha pausado la ejecución, pulse ok para continuar");
 
-
+}
--- js/loadword.js
+++ js/loadword.js
@@ -2,37 +2,122 @@
 function loadword(){
     
     var word = document.getElementById('Word').value;
-    
+
     $("#estado").fadeIn(function() {
         $(this).html("Estado: Ninguno ").fadeIn();
       });
 
     borrador();
 
-    borrador();
+
     
     if(word != ""){
         var items = document.getElementsByClassName('item');
-        cont = 9 ;
+        cont = 11;
+        resaltar = 12;
+        $("#cuadro" + resaltar).fadeIn(function () {
+            $(this).css("backgroundColor", "#951197 ");
+        });
         for(i=0; i<word.length; i++){
             if(word.charAt(i)=="a" || word.charAt(i)=="b"){
-                items[cont+1].innerHTML = word[i];
+                $(items[cont+1]).html("<h1>"+word[i]+"</h1>");
                 cont++;
             }else{
                 borrador();
-                window.location.href = "error.html";
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'La palabra cargada no es valida',
+                    confirmButtonText: 'Aceptar',
+                    //footer: '<a href="../../vista/pagina/login.php">Why do I have this issue?</a>'
+                  }).then((result) => {
+                    if (result.value) {
+                      clearFileInput(document.getElementById("Word"));
+                      document.getElementById("Word").focus();
+                      Swal.close("salir");
+                      }
+                  });
+               // window.location.href = "error.html";
+               Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'datos ingresados incorrectamente',
+                confirmButtonText: 'Aceptar',
+                //footer: '<a href="../../vista/pagina/login.php">Why do I have this issue?</a>'
+              }).then((result) => {
+  if (result.value) {
+    window.location.href="index.html";
+  }
+})
+
                 break;
             }
         }
     }
     else{
-        window.location.href = "error.html";
+
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'No ingresó ninguna palabra',
+            confirmButtonText: 'Aceptar',
+            //footer: '<a href="../../vista/pagina/login.php">Why do I have this issue?</a>'
+          }).then((result) => {
+            if (result.value) {
+                document.getElementById("Word").focus();
+              Swal.close("salir");
+            }
+          });
+
+       // window.location.href = "error.html";
+       Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Esta vacio! vuelva e ingrese de nuevo la palabra',
+        confirmButtonText: 'Aceptar',
+        //footer: '<a href="../../vista/pagina/login.php">Why do I have this issue?</a>'
+      }).then((result) => {
+if (result.value) {
+window.location.href="index.html";
+}
+})
+
     }
 }
 function borrador() {
     var items = document.getElementsByClassName('item'); 
     for(i=0;i<=100; i++){
-        items[i].innerHTML = "";   
-        }       
+        items[i].innerHTML = "";
+    }
 }
+function refresk(){
+  Swal
+  .fire({
+      title: "Quiere detener la ejecución",
+      text: "¿Detener?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: "Sí,detener",
+      cancelButtonText: "Cancelar",
+  })
+  .then(resultado => {
+      if (resultado.value) {
+          // Hicieron click en "Sí"
+          location.reload();
+          
+      } else {
+          // Dijeron que no
+          console.log("NO se elimina la venta");
+      }
+  });
+  
 
+}
+function clearFileInput(ctrl) {
+  try {
+    ctrl.value = null;
+  } catch(ex) { }
+  if (ctrl.value) {
+    ctrl.parentNode.replaceChild(ctrl.cloneNode(true), ctrl);
+  }
+}
